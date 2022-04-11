c9c0d03af4ecd8f68f1c3a4131e9536f85a5c63e
Mitch Toran <mitchellht34@gmail.com>
2022-04-11 13:35:05 -0400

    add comics delete and post routes

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -5,4 +5,44 @@ class ComicsController < ApplicationController
         comics.to_json
     end
 
+    get "/comics/:id" do
+        comic = Comic.find(params[:id])
+        if comic
+            comic.to_json
+        else
+            {error: "Comic does not exist"}.to_json
+        end
+    end
+
+    post "/comics do" do
+        comic = Comic.create(
+            title: params[:title],
+            publisher: params[:publisher],
+            issues: params[:issues],
+            release_date: params[:release_date],
+            synopsis: params[:synopsis],
+            writer: params[:writer],
+            artist: params[:artist],
+            character_id: params[:character_id]
+        )
+        if comic.id
+            comic.to_json
+        else
+            {error: comic.errors.full_messages.to_sentence}.to_json
+        end
+    end
+
+    delete "/comics/:id" do
+        comic = Comic.find(params[:id])
+        if comic
+            comic.destroy
+            "Successfully Deleted Comic"
+        else
+            {error: "Comic does not exist"}.to_json
+        end
+    end
+
+
+
+
 end
