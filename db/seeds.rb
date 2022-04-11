be2734d39b42a0a519110e42376386ad4480f782
Mitch Toran <mitchellht34@gmail.com>
2022-04-11 11:59:06 -0400

    correct seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,153 @@ Comic.create(
     artist: "Greg Capullo"
 )
 
-# Comic.create(
-#     title: "",
-#     publisher: "",
-#     issues: ,
-#     release_date: ,
-#     synopsis: "",
-#     writer: "",
-#     artist: ""
-# )
+Comic.create(
+    title: "Red Hood and the Outlaws",
+    publisher: "DC Comics",
+    issues: 52,
+    release_date: 2016,
+    synopsis: "When a shocking encounter with Batman solidifies the Red Hood's status as a villain, Jason Todd goes deep undercover to take down Gotham City's criminal underworld from the inside. Along the way, Jason meets two unlikely allies: a disgraced Amazon warrior named Artemis and a half-baked Superman clone called Bizarro—and the DCU's ""Dark Trinity"" is born!",
+    writer: "Scott Lobdell",
+    artist: "Dexter Soy"
+)
+
+Comic.create(
+    title: "Grayson",
+    publisher: "DC Comics",
+    issues: 20,
+    release_date: 2014,
+    synopsis: "Following the events of FOREVER EVIL, Nightwing is no more! Now, there is only Grayson, international man of mystery for Spyral. One of DC Comics' most popular characters gets a major makeover and an all-new direction for The New 52.",
+    writer: "Tom King",
+    artist: "Mikel Janin"
+)
+
+Comic.create(
+    title: "Future State: Superman - House of El",
+    publisher: "DC Comics",
+    issues: 1,
+    release_date: 2021,
+    synopsis: "Centuries into the future, the bloodline of Kal-El, the hero we know as Clark Kent, continues. Meet a new generation of Kryptonian heroes as they stand against one of the greatest threats they've ever faced: a diabolical foe called the Red King. Get ready for the unexpected debuts of the twins known as Rowan and Ronan Kent, descendants of Jonathan Kent. Rowan is the new Superman of Earth, while his sister is a Blue Lantern. Also on board are Theand'r Ban-El, whose mother was Tamaranean, and other heroes—all led by the original Man of Steel himself!",
+    writer: "Phillip Kennedy Johnson",
+    artist: "Scott Godlewski"
+)
+
+Comic.create(
+    title: "Green Lantern",
+    publisher: "DC Comics",
+    issues: 67,
+    release_date: 2005,
+    synopsis: "Hal Jordan is back as Earth's Green Lantern, rebuilding the life he left behind while protecting his sector from intergalactic threats, written by comics superstar GEOFF JOHNS.",
+    writer: "Geoff Johns",
+    artist: "Carlos Pacheco"
+)
+
+Comic.create(
+    title: "Green Lantern",
+    publisher: "DC Comics",
+    issues: 52,
+    release_date: 2011,
+    synopsis: "In DC Comics' Green Lantern comic book series, a deadly showdown between the Green Lantern Corps and a mysterious foe from the past leaves Hal Jordan without his ring. Discover the adventures of Hal, Sinestro, and the other Green Lanterns.",
+    writer: "Geoff Johns",
+    artist: "Doug Mahnke"
+)
+
+Comic.create(
+    title: "Invincible",
+    publisher: "Image Comics",
+    issues: 144,
+    release_date: 2003,
+    synopsis: "Mark Grayson is the teenage superhero known as Invincible. He was a normal high school senior with a normal part-time job and otherwise normal life, except for the fact that his father Nolan is the superhero Omni-Man, the most powerful superhero on the planet.",
+    writer: "Robert Kirkman",
+    artist: "Cory Walker"
+)
+
+Comic.create(
+    title: "BRZRKR",
+    publisher: "BOOM! Studios",
+    issues: 9,
+    release_date: 2021,
+    synopsis: "The man known only as Berzerker is half-mortal and half-God, cursed and compelled to violence…even at the sacrifice of his sanity. But after wandering the world for centuries, Berzerker may have finally found a refuge – working for the U.S. government to fight the battles too violent and too dangerous for anyone else. In exchange, Berzerker will be granted the one thing he desires – the truth about his endless blood-soaked existence… and how to end it.",
+    writer: "Keanu Reeves",
+    artist: "Ron Garney"
+)
+Comic.create(
+    title: "Wolverine: Old Man Logan",
+    publisher: "Marvel Comics",
+    issues: 8,
+    release_date: 2009,
+    synopsis: "MARK MILLAR and STEVE MCNIVEN-who last teamed for the monumental CIVIL WAR-bring us the most important WOLVERINE story of the 21st Century. Nobody knows what happened on the night the heroes fell. All we know is that they disappeared and evil triumphed and the bad guys have been calling the shots ever since. What happened to Wolverine is the biggest mystery of all. For 50 years, no one has heard hide nor hair from him...and in his place stands an old man called Logan. A man concerned only about his family. A man pushed to the brink by the HULK GANG. A man forced to help an old friend-the blind archer, HAWKEYE-to drive three thousand miles to secure his family's safety. Get ready for the ride of your life, Logan.",
+    writer: "Mark Millar",
+    artist: "Steve McNiven"
+)
+
+Comic.create(
+    title: "Ultimate Spider-Man",
+    publisher: "Marvel Comics",
+    issues: 133,
+    release_date: 2000,
+    synopsis: "A new beginning for Spider-man started in the popular Ultimate Spider-man comic-series. Re-imagined for the new century!",
+    writer: "Brian Michael Bendis",
+    artist: "Mark Bagley"
+)
+Comic.create(
+    title: "The Walking Dead",
+    publisher: "Image Comics",
+    issues: 193,
+    release_date: 2003,
+    synopsis: "Robert Kirkman and Image Comics' fan-favorite comic book series, The Walking Dead, follows Rick Grimes, a small-town police officer from Cynthiana, Kentucky, his family, and a number of other survivors who have banded together in order to survive after the world is overrun with zombies.",
+    writer: "Robert Kirkman",
+    artist: "Tony Moore"
+)
+
+Comic.create(
+    title: "Thor",
+    publisher: "Marvel Comics",
+    issues: 26,
+    release_date: 2020,
+    synopsis: "A NEW LEGEND BEGINS! The prince is now a king. All of Asgard lies before Thor, the God of Thunder, and the Ten Realms are finally at peace. But the skies above the Realm Eternal are never clear for long. The Black Winter is coming — and to triumph over this new threat, Thor must be transformed in a most unexpected way! Bursting with new power and set on a dangerous path, Thor will battle friend and foe alike. But can he convince Beta Ray Bill to let him continue his bloody mission to save all that is? The Black Winter has the power to reveal the means of any person’s demise. Thor will glimpse his future — and if the vision is true, Asgard will soon need a new Odinson to take the throne!",
+    writer: "Donny Cates",
+    artist: "Nic Klein"
+)
+
+Comic.create(
+    title: "Thor",
+    publisher: "Marvel Comics",
+    issues: 16,
+    release_date: 2018,
+    synopsis: "Written by Jason Aaron with art by Mike Del Mundo, sees the start of a new mission for the God of Thunder. The artifacts of Asgard have been scattered across the Earth, and to reclaim them, Thor will have to face some ugly truths…Like the production cost of hundreds of new hammers!",
+    writer: "Jason Aaron",
+    artist: "Michael del Mundo"
+)
+
+Comic.create(
+    title: "Venom",
+    publisher: "Marvel Comics",
+    issues: 35,
+    release_date: 2018,
+    synopsis: "In the wake of S.H.I.E.L.D.’s collapse, an ancient and primordial evil has been awakened beneath the streets of New York, and with it, something equally evil has awakened in that most Wicked of Webslingers – VENOM! Still a Lethal Protector of the innocents in New York, this never-before-seen threat could force Venom to relinquish everything it holds dear – including Eddie Brock! Join two of the hottest creators in comics today, Donny Cates and Ryan Stegman, for a VENOM adventure a thousand years in the making!",
+    writer: "Donny Cates",
+    artist: "Ryan Stegman"
+)
+
+Comic.create(
+    title: "Spawn",
+    publisher: "Image Comics",
+    issues: 331,
+    release_date: 1992,
+    synopsis: "While a strange assailant stalks the city, ripping out human hearts, another otherwordly being arrives. As his mind reels, our tortured hero remembers that he struck a deal with the devil in order to return to his beloved wife - five years after his death.",
+    writer: "Todd McFarlane",
+    artist: "Greg Capullo"
+)
+
+Comic.create(
+    title: "Detective Comics",
+    publisher: "DC Comics",
+    issues: 52,
+    release_date: 2011,
+    synopsis: "DC's flagship title re-launches for the first time ever, featuring Batman adventures from the acclaimed Tony Daniel (BATMAN R.I.P.)! Bruce Wayne is back under the cape and cowl, ready to protect Gotham City from its deadly array of villains--from the Joker to Mr. Freeze to brand new masked murderers!",
+    writer: "Tony S. Daniel",
+    artist: "Ryan Winn"
+)
 
 # Comic.create(
 #     title: "",
