0251aaef6929d09f4db132c4f34bb69e722e1630
Mitch Toran <mitchellht34@gmail.com>
2022-04-10 20:46:13 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,53 @@ ComicBook.create(
     number_of_issues: 3
 )
 
+ComicBook.create(
+    title: "Teen Titans Academy",
+    publisher: "DC Comics",
+    synopsis: "Welcome to Teen Titans Academy! Packed with both new, super-powered teens and numerous dark secrets, Teen Titans Academy’s student body includes Shazam; a new Australian speedster; a trio of Goth(am) teen expatriates obsessed with Batman—and one member of this first class will become the deadly Red X. Original New Teen Titans including Nightwing, Starfire, Raven, Cyborg, and Beast Boy take on the role of teachers and mentors for superpowered teens. Their goal: to shape the next generation of heroes.",
+    release_date: 2021,
+    number_of_issues: 16
+)
+
+ComicBook.create(
+    title: "Thor",
+    publisher: "Marvel Comics",
+    synopsis: "Written by Jason Aaron with art by Mike Del Mundo, sees the start of a new mission for the God of Thunder. The artifacts of Asgard have been scattered across the Earth, and to reclaim them, Thor will have to face some ugly truths…Like the production cost of hundreds of new hammers!",
+    release_date: 2018,
+    number_of_issues: 16
+)
+
+ComicBook.create(
+    title: "Thor",
+    publisher: "Marvel Comics",
+    synopsis: "A NEW LEGEND BEGINS! The prince is now a king. All of Asgard lies before Thor, the God of Thunder, and the Ten Realms are finally at peace. But the skies above the Realm Eternal are never clear for long. The Black Winter is coming — and to triumph over this new threat, Thor must be transformed in a most unexpected way! Bursting with new power and set on a dangerous path, Thor will battle friend and foe alike. But can he convince Beta Ray Bill to let him continue his bloody mission to save all that is? The Black Winter has the power to reveal the means of any person’s demise. Thor will glimpse his future — and if the vision is true, Asgard will soon need a new Odinson to take the throne!",
+    release_date: 2020,
+    number_of_issues: 26
+)
+
+ComicBook.create(
+    title: "American Vampire",
+    publisher: "Vertigo Comics",
+    synopsis: "In DC and Vertigo's American Vampire, writers Scott Snyder and the legendary Stephen King, alongside a bevy of artists, present the birth of a brand new species of vampire! When notorious outlaw Skinner Sweet is attacked by an old enemy (who happens to be a member of the undead), the first American vampire is born... a vampire powered by the sun; stronger, fiercer, and meaner than anything that came before.",
+    release_date: 2010,
+    number_of_issues: 34
+)
+
+ComicBook.create(
+    title: "BRZRKR",
+    publisher: "BOOM! Studios",
+    synopsis: "The man known only as Berzerker is half-mortal and half-God, cursed and compelled to violence…even at the sacrifice of his sanity. But after wandering the world for centuries, Berzerker may have finally found a refuge – working for the U.S. government to fight the battles too violent and too dangerous for anyone else. In exchange, Berzerker will be granted the one thing he desires – the truth about his endless blood-soaked existence… and how to end it.",
+    release_date: 2021,
+    number_of_issues: 9
+
+    )
+ComicBook.create(
+    title: "Invincible",
+    publisher: "Image Comics",
+    synopsis: "Mark Grayson is the teenage superhero known as Invincible. He was a normal high school senior with a normal part-time job and otherwise normal life, except for the fact that his father Nolan is the superhero Omni-Man, the most powerful superhero on the planet.",
+    release_date: 2003,
+    number_of_issues: 144
+)
+
+
 puts "✅ Done seeding!"
