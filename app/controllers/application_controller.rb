e75abf1d40f12573ca04b2030d6ed38ea008f382
Mitch Toran <mitchellht34@gmail.com>
2022-04-11 13:16:45 -0400

    seperate controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,11 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
+   
   # Add your routes here
-  get "/comics" do
-    comics = Comic.all
-    comics.to_json
+  get "/" do
+    { message: "Good luck with your project!" }.to_json
   end
-  
-  get "/characters" do
-    characters = Character.all
-    characters.to_json
-  end
+
 
 end
