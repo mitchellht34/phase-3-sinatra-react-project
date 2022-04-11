a9405b036b1b3da21af5d355456aeba75d53bc16
Mitch Toran <mitchellht34@gmail.com>
2022-04-10 20:19:24 -0400

    create associations

--- app/models/comic_book.rb
+++ app/models/comic_book.rb
@@ -1,3 +1,4 @@
 class ComicBook < ActiveRecord::Base
     has_many :issues
+    has_many :characters, through: :issues
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,4 @@
 class Issue < ActiveRecord::Base
+    belongs_to :comic_book
+    has_many :characters
 end
